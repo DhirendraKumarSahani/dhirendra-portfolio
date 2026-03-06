e03d9ead6f0fd2a880db54dc84001ac15ff87e13
Dhirendra Kumar <dhirendraroy8882414182@gmail.com>
2026-03-06 09:31:32 +0530

    Fix sqlite ssl issue

--- core/settings.py
+++ core/settings.py
@@ -85,8 +85,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR}/db.sqlite3",
-        conn_max_age=600,
-        ssl_require=not DEBUG
+        conn_max_age=600
     )
 }
 
